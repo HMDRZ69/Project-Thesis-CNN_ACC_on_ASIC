37768d02af11c2e529a7ec23e761135bcf58c140
Hamed Ramezanzadeh <hamed.rmz1990@gmail.com>
2026-05-30 11:20:43 +0200

    cleanup: final clean repository structure

--- Cadence/Genus/Genus_Out/genus_sram.cmd
+++
@@ -1,8 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 23.11-s100_1, built Apr 22 2024 11:28:10
-
-# Date: Mon May 25 10:01:12 2026
-# Host: ei-vm-018.othr.de (x86_64 w/Linux 4.18.0-553.123.1.el8_10.x86_64) (32cores*32cpus*1physical cpu*AMD Ryzen Threadripper PRO 7975WX 32-Cores 512KB)
-# OS:   Red Hat Enterprise Linux release 8.10 (Ootpa)
-
-source run_genus.tcl
-exit
