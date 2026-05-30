fc19c714d97a77fa4464d23793804c67fed479e7
Hamed Ramezanzadeh <hamed.rmz1990@gmail.com>
2026-05-30 11:21:32 +0200

    cleanup: final clean repository structure

--- rtl/halfadder.sv
+++
@@ -1,9 +0,0 @@
-// half_adder.v
-// Simple 2-input 2-output half adder module written by HMD
-
-module half_adder(input A, input B, output SUM, output CARRY);
-
-    assign SUM = A ^ B;   //XOR Operation
-    assign CARRY = A & B;   //AND Operation
-
-endmodule
